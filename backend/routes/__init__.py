033c6bd428a2c8d9f07e9ee6c7bcbe4da4d5a7c6
BMDcoder <mwitabryson@gmail.com>
2025-08-09 16:32:25 +0300

    Update __init__.py

--- backend/routes/__init__.py
+++ backend/routes/__init__.py
@@ -1,3 +1,3 @@
-from admin_agents import router as admin_agents
-from auth import router as auth
-from chat import router as chat
+from .admin_agents import router as admin_agents
+from .auth import router as auth
+from .chat import router as chat
