a0d4daec76dd83ab7fc5c9f64fcccd1ff43d3b2f
BMDcoder <mwitabryson@gmail.com>
2025-08-10 18:05:36 +0300

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -6,17 +6,6 @@ load_dotenv()
 # Railway sets DATABASE_URL as environment variable like this:
 DATABASE_URL = os.getenv("DATABASE_URL", os.getenv("POSTGRES_DATABASE_URL"))
 
-# If not found, build it from individual PG* vars (Railway provides these by default)
-if not DATABASE_URL:
-    PGUSER = os.getenv("PGUSER", "postgres")
-    PGPASSWORD = os.getenv("PGPASSWORD", "")
-    PGHOST = os.getenv("PGHOST", "localhost")
-    PGPORT = os.getenv("PGPORT", "5432")
-    PGDATABASE = os.getenv("PGDATABASE", "postgres")
-
-    DATABASE_URL = f"postgresql+psycopg2://{PGUSER}:{PGPASSWORD}@{PGHOST}:{PGPORT}/{PGDATABASE}"
-
-
 SECRET_KEY = os.getenv("SECRET_KEY", "devsecret")
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", "1440"))
