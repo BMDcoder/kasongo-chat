eea718ad2bf15b9b25d3067501c7177f01ef52cb
BMDcoder <mwitabryson@gmail.com>
2025-08-10 16:21:17 +0300

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -3,9 +3,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# Railway sets DATABASE_URL as environment variable like this:
-DATABASE_URL = os.getenv("DATABASE_URL", os.getenv("POSTGRES_DATABASE_URL"))
-
 SECRET_KEY = os.getenv("SECRET_KEY", "devsecret")
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", "1440"))
@@ -13,6 +10,9 @@ OPENAI_KEY = os.getenv("OPENAI_API_KEY", "")
 HF_API_TOKEN = os.getenv("HF_API_TOKEN", "")
 COHERE_API_KEY = os.getenv("COHERE_API_KEY", "")
 
+
+# Railway sets DATABASE_URL as environment variable like this:
+DATABASE_URL = os.getenv("DATABASE_URL", os.getenv("POSTGRES_DATABASE_URL"))
 
 # If not found, build it from individual PG* vars (Railway provides these by default)
 if not DATABASE_URL:
