4049dbb090322ad8887ff12f3ee1248dde89c52e
BMDcoder <mwitabryson@gmail.com>
2025-08-11 05:34:04 +0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -144,7 +144,7 @@ const styles = {
     backgroundColor: "rgba(255,255,255,0.85)",
     borderRadius: 12,
     boxShadow: "0 8px 24px rgba(0,0,0,0.15)",
-    width: "96%",
+    width: "98%",
     maxWidth: 800,
     display: "flex",
     flexDirection: "column",
